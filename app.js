88a8b4acf6af9945c6eb2d32491463387d9e587e
zuzuwi <zuzuwi>
2023-10-27 16:47:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// function changeColor(event){
+//     let code = event.keyCode || event.which;
+//     let body= document.querySelector("body")
+
+//     if(code ==77) { // M harfi
+//         console.log("M harfini bosdingiz")
+//     }
+//    else if(code ==78) { // N harfi
+//         alert("N harfini bosdingiz")
+//     }
+//     else if(code ==79) { // O harfi
+//         body.innerHTML = "O harfini bosdizgiz"
+//     }
+//     else{
+//         body.style.backgroundClip = 'white'
+//     }
+// }
+
+function functionBoll(event){
+    let code = event.keyCode || event.which;
+    let body= document.getElementById("boll")
+
+    if(code == 62 ){
+        body.style. margin-left "100px"
+    }
+    else if(code == 60){
+
+    }
+}
